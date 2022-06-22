7209e8741487a02acad526b434cd4afc501bb8c8
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-06-22 16:36:36 +0500

    🎨 basic structure is created files and folder without content

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './state/Configurestore';
-const container = document.getElementById('root')!;
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./state/configureStore";
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-    
   </React.StrictMode>
 );
 
