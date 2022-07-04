df718382bc38c878e70e310d0a88fabe5913d1d6
Muhammad Ahmad Faraz <ahmadfaraz629@gmail.com>
2022-07-04 17:30:20 +0500

    Update src/state/ducks/student/reducer.ts

--- src/state/ducks/student/reducer.ts
+++ src/state/ducks/student/reducer.ts
@@ -4,12 +4,6 @@ export const userSlice = createSlice({
   name: "student",
   initialState: { records: [] },
   reducers: {
-    add: (state: any) => {
-      state.records.push(1);
-    },
-    del: (state: any) => {
-      state.records.pop(1);
-    },
   },
 });
 export const { add, del } = userSlice.actions;
