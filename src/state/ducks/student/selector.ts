3e6cfe87c320ba9c50a0cf3818c717fa768f7fd6
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-07-27 12:04:29 +0500

    ✨ lowest grade implemented

--- src/state/ducks/student/selector.ts
+++ src/state/ducks/student/selector.ts
@@ -12,13 +12,22 @@ export const getStudentSummary = createSelector(getList, (list) => {
 
   for (let item of list) {
     if (gradesPresidance.indexOf(item.grade) !== 0) {
+      if (lowestGrade === "--") lowestGrade = item.grade;
+      else if (
+        gradesPresidance.indexOf(item.grade) <
+        gradesPresidance.indexOf(lowestGrade)
+      )
+        lowestGrade = item.grade;
       if (highestGrade === "--") highestGrade = item.grade;
       else if (
         gradesPresidance.indexOf(item.grade) >
         gradesPresidance.indexOf(highestGrade)
       )
         highestGrade = item.grade;
+    } else {
+      lowestGrade = item.grade;
     }
   }
   console.log("highest grade", highestGrade);
+  console.log("lowest grade", lowestGrade);
 });
